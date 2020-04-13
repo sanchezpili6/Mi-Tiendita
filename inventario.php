f472df80a6c7258001bd485e8af14e1f41d01619
MrCasco <ernesto.casco.velazquez@hotmail>
2020-04-13 15:15:04 -0500

    Cambio de html a php

--- inventario.php
+++ inventario.php
@@ -4,27 +4,43 @@
     <link href="bootstrap.min.css" rel="stylesheet">
     <link href="dashboard.css" rel="stylesheet">
     <meta charset="utf-8">
-    <title></title>
+    <link rel="icon" href="imágenes/logo_size.jpg">
+    <title>Inventario</title>
   </head>
   <body>
-    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Mi Tiendita</a>
+    <!--Barra de navegaciónde arriba-->
+    <nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.html">Mi Tiendita</a>
+      <div class="collapse navbar-collapse sticky-top" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Informes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="cajero.html">Cajero</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Registar usuario</a>
+          </li>
+      </ul>
+      </div>
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
           <a class="nav-link" href="#">Cerrar sesión</a>
         </li>
       </ul>
     </nav>
-
+    <!--Barra de navegaciónde arriba-->
+    <!--Barra de la izquierda-->
     <div class="container-fluid">
       <div class="row">
         <nav class="col-md-2 d-none d-md-block bg-light sidebar">
           <div class="sidebar-sticky">
             <ul class="nav flex-column">
               <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <a class="nav-link" href="dashboard.html">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-                  Dashboard <span class="sr-only">(current)</span>
+                  Dashboard
                 </a>
               </li>
               <li class="nav-item">
@@ -34,9 +50,9 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="inventario.php">
+                <a class="nav-link active" href="inventario.html">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
-                  Productos
+                  Productos <span class="sr-only">(current)</span>
                 </a>
               </li>
               <li class="nav-item">
@@ -89,8 +105,10 @@
         </nav>
       </div>
     </div>
-
+    <!--Barra de la izquierda-->
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
     <table class="table container-fluid">
+      <div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
       <thead class="thead-dark">
         <tr>
           <th>Id</th>
@@ -122,6 +140,7 @@
       mysqli_close($con);
       ?>
     </table>
+  </main>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="../../assets/js/vendor/popper.min.js"></script>
