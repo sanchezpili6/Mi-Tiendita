7503fa2dee3753b681256a9c97598d1b524d014f
MrCasco <ernesto.casco.velazquez@hotmail>
2020-04-27 19:01:07 -0500

    Link de páginas con su respectivo php

--- login.php
+++ login.php
@@ -1,10 +1,12 @@
 <?php
 $email = $_REQUEST['email'];
 $password = $_REQUEST['password'];
+echo $email . $password;
 if ($email === 'casco@casco.com' && $password === 'casco') {
   header('Location: http://localhost/Mi-Tiendita/dashboard.html');
 }
 else {
+  //header('Location: http://localhost/Mi-Tiendita/inicio_sesion.html');
   echo "Wrong username or password";
 }
  ?>
