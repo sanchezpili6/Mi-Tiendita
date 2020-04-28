dde646dd2f89f09eb2d6626dd2b671022cbbc7fd
MrCasco <ernesto.casco.velazquez@hotmail>
2020-04-27 20:01:37 -0500

    Ya jala la venta y actualiza la db

--- nuevaVenta.php
+++ nuevaVenta.php
@@ -2,38 +2,44 @@
   include "db.php";
   global $con;
 
-  // $query = "SELECT * FROM productos WHERE nombre='Chokis'";
-  // $res = mysqli_query($con, $query);
-  //
-  // while($row = mysqli_fetch_assoc($res)){
-  //   $newValue = $row['existencias'] - 1;
-  //   $sqlUpdate = "UPDATE productos SET existencias=$newValue WHERE nombre = 'Chokis'";
-  //   $query = mysqli_query($con, $sqlUpdate);
-  // }
-  //
-  // $query = "SELECT * FROM productos WHERE nombre = 'Choko Chokis'";
-  // $res = mysqli_query($con, $query);
-  //
-  // while($row = mysqli_fetch_assoc($res) ){
-  //   $newValue = $row['existencias'] - 1;
-  //   $sqlUpdate = "UPDATE productos SET existencias=$newValue WHERE nombre = 'Choko Chokis'";
-  //   $query = mysqli_query($con, $sqlUpdate);
-  // }
-  //
-  // $query = "SELECT * FROM productos WHERE nombre = 'helado hersheys'";
-  // $res = mysqli_query($con, $query);
-  //
-  // while($row = mysqli_fetch_assoc($res) ){
-  //   $newValue = $row['existencias'] - 1;
-  //   $sqlUpdate = "UPDATE productos SET existencias=$newValue WHERE nombre = 'helado hersheys'";
-  //   $query = mysqli_query($con, $sqlUpdate);
-  // }
+  $first = $_COOKIE['Nito'];
+  $second = $_COOKIE['Chokis'];
+  $third = $_COOKIE['ChokoChokis'];
+  $fourth = $_COOKIE['HeladoHersheys'];
 
-  $first = $_REQUEST['first'];
-  $second = $_REQUEST['second'];
-  $third = $_REQUEST['third'];
+  $query = "SELECT * FROM productos WHERE nombre='Nito'";
+  $res = mysqli_query($con, $query);
+  while($row = mysqli_fetch_assoc($res)){
+    $newValue = $row['existencias'] - $first;
+    $sqlUpdate = "UPDATE productos SET existencias=$newValue WHERE nombre='Nito'";
+    $query = mysqli_query($con, $sqlUpdate);
+  }
+
+  $query = "SELECT * FROM productos WHERE nombre='Chokis'";
+  $res = mysqli_query($con, $query);
+  while($row = mysqli_fetch_assoc($res)){
+    $newValue = $row['existencias'] - $second;
+    $sqlUpdate = "UPDATE productos SET existencias=$newValue WHERE nombre = 'Chokis'";
+    $query = mysqli_query($con, $sqlUpdate);
+  }
+
+  $query = "SELECT * FROM productos WHERE nombre='Choko Chokis'";
+  $res = mysqli_query($con, $query);
+  while($row = mysqli_fetch_assoc($res)){
+    $newValue = $row['existencias'] - $third;
+    $sqlUpdate = "UPDATE productos SET existencias=$newValue WHERE nombre = 'Choko Chokis'";
+    $query = mysqli_query($con, $sqlUpdate);
+  }
+
+  $query = "SELECT * FROM productos WHERE nombre='helado hersheys'";
+  $res = mysqli_query($con, $query);
+  while($row = mysqli_fetch_assoc($res)){
+    $newValue = $row['existencias'] - $fourth;
+    $sqlUpdate = "UPDATE productos SET existencias=$newValue WHERE nombre = 'helado hersheys'";
+    $query = mysqli_query($con, $sqlUpdate);
+  }
 
   mysqli_close($con);
 
-  //header("Location: http://localhost/Mi-Tiendita/cajero.html");
+  header("Location: http://localhost/Mi-Tiendita/cajero.html");
  ?>
