4c162f05ad508c4ca7da6e7e2a20d99a778822cc
MrCasco <ernesto.casco.velazquez@hotmail>
2020-05-11 11:54:17 -0500

    Redireccion a archivos php, no html

--- inventario.php
+++ inventario.php
@@ -17,10 +17,10 @@
             <a class="nav-link" href="#">Informes</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="cajero.html">Cajero</a>
+            <a class="nav-link" href="cajero.php">Cajero</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="registroper.html">Registar usuario</a>
+            <a class="nav-link" href="registroper.php">Registar usuario</a>
           </li>
       </ul>
       </div>
@@ -50,7 +50,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" href="inventario.html">
+                <a class="nav-link active" href="inventario.php">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
                   Productos <span class="sr-only">(current)</span>
                 </a>
