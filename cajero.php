63157b57baa52009f9ef5ce5b01bb456d00090ee
MrCasco <ernesto.casco.velazquez@hotmail>
2020-05-11 18:15:30 -0500

    Link con las respectivas páginas php

--- cajero.php
+++ cajero.php
@@ -52,7 +52,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="nuevosProductos.php">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
                   Registrar inventario
                 </a>
@@ -65,7 +65,6 @@
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="inventario.php">
-                <a class="nav-link" href="inventario.html">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
                   Ver inventario
                 </a>
