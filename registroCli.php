9cd4b05dad6a2f88df75c77b560c8a3928fb063c
MrCasco <ernesto.casco.velazquez@hotmail>
2020-05-11 18:32:22 -0500

    Ya sale correo en clientes.php

--- registroCli.php
+++ registroCli.php
@@ -8,7 +8,8 @@
   $mail = $_REQUEST['email'];
   $password = $_REQUEST['password'];
 
-  $query = "INSERT INTO clientes (id, nombre, fecha, correo, password) values($id, '".$nombre."', '".$fechaRegistro."', '".$fechaRegistro."', '".$password."')";
+  $query = "INSERT INTO clientes (id, nombre, fecha, correo, password) values($id, '".$nombre."', '".$fechaRegistro."', '".$mail."', '".$password."')";
   $res = mysqli_query($con, $query);
   echo mysqli_error($con);
+  header("Location: http://localhost/Mi-Tiendita/clientes.php");
 ?>
