c2f095b942e5e4033d7df8c3e2751a68aa3c63e4
MrCasco <ernesto.casco.velazquez@hotmail>
2020-06-09 14:10:20 -0500

    Redirección a dashboard.html

--- ventas.php
+++ ventas.php
@@ -14,7 +14,7 @@
       <div class="collapse navbar-collapse sticky-top" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Informes</a>
+            <a class="nav-link" href="dashboard.html">Informes</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="cajero.php">Cajero</a>
